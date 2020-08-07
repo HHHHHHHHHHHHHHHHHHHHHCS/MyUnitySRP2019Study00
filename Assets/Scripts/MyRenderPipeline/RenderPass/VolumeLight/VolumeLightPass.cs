1443d39404b855e0d78579666439d1a8219f5863
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-08-07 15:59:56 +0800

    add

---
+++ Assets/Scripts/MyRenderPipeline/RenderPass/VolumeLight/VolumeLightPass.cs
@@ -0,0 +1,215 @@
+﻿using System.Collections.Generic;
+using MyRenderPipeline.Utils;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Utility;
+
+namespace MyRenderPipeline.RenderPass.VolumeLight
+{
+	[CreateAssetMenu(fileName = "VolumeLight", menuName = "MyRP/RenderPass/VolumeLight")]
+	public class VolumeLightPass : MyRenderPassAsset
+	{
+		public int volumeResolutionScale = 2;
+		public float visibilityDistance = 20;
+
+		[ColorUsage(false, true)] public Color fogLight;
+		public Material material;
+		public Texture2D[] jitterPatterns;
+
+		public override MyRenderPass CreateRenderPass()
+		{
+			return new VolumeLightRender(this);
+		}
+	}
+
+	public class VolumeLightRender : MyRenderPassRenderer<VolumeLightPass>
+	{
+		private struct LightVolumeData
+		{
+			public VolumeLightRenderer volume;
+			public int lightIndex;
+			public int volumeIndex;
+		}
+
+		private const int volumeDepthPass = 0;
+		private const int volumeScatteringPass = 1;
+		private const int fullScreenVolumeScatteringPass = 2;
+		private const int volumeResolvePass = 3;
+		private const int globalFogPass = 4;
+
+		private List<LightVolumeData> visibleVolumes = new List<LightVolumeData>();
+		private int volumeDepthTexID = -1;
+
+		private Material volumeMat => asset.material;
+
+		public VolumeLightRender(VolumeLightPass asset) : base(asset)
+		{
+			volumeDepthTexID = Shader.PropertyToID("_VolumeDepthTexture");
+		}
+
+		public override void Setup(ScriptableRenderContext context, ref MyRenderingData renderingData)
+		{
+			visibleVolumes.Clear();
+
+			for (int i = 0; i < renderingData.cullResults.visibleLights.Length; i++)
+			{
+				var light = renderingData.cullResults.visibleLights[i];
+				VolumeLightRenderer vlr = light.light.GetComponent<VolumeLightRenderer>();
+				if (vlr)
+				{
+					visibleVolumes.Add(new LightVolumeData()
+					{
+						lightIndex = i,
+						volumeIndex = visibleVolumes.Count,
+						volume = vlr,
+					});
+				}
+			}
+
+			// 第一帧数 初始化
+			if (renderingData.frameID == 0)
+			{
+				var cmd = CommandBufferPool.Get();
+				cmd.SetGlobalVectorArray("_BoundaryPlanes", new Vector4[6]);
+				context.ExecuteCommandBuffer(cmd);
+				cmd.Clear();
+				CommandBufferPool.Release(cmd);
+			}
+		}
+
+		public override void Render(ScriptableRenderContext context, ref MyRenderingData renderingData)
+		{
+			var cmd = CommandBufferPool.Get("Volume Light");
+
+			using (new ProfilingSample(cmd, "Volume Light"))
+			{
+				RenderVolumeLight(cmd, renderingData);
+
+				context.ExecuteCommandBuffer(cmd);
+				cmd.Clear();
+				CommandBufferPool.Release(cmd);
+			}
+		}
+
+		//Debug 用
+		private void RenderVolumeDepth(CommandBuffer cmd, MyRenderingData renderingData)
+		{
+			var debugRT = IdentifierPool.Get();
+			cmd.GetTemporaryRT(debugRT, renderingData.camera.pixelWidth, renderingData.camera.pixelHeight, 0,
+				FilterMode.Point, RenderTextureFormat.ARGBFloat);
+			cmd.SetRenderTarget(debugRT);
+			cmd.SetGlobalTexture("_RWVolumeDepthTexture", volumeDepthTexID);
+
+			foreach (var volumeData in visibleVolumes)
+			{
+				cmd.SetGlobalInt("_VolumeIndex", volumeData.volumeIndex);
+				cmd.DrawMesh(volumeData.volume.VolumeMesh, volumeData.volume.transform.localToWorldMatrix, volumeMat, 0,
+					volumeDepthPass);
+			}
+
+			cmd.ReleaseTemporaryRT(debugRT);
+			IdentifierPool.Release(debugRT);
+		}
+
+
+		private void RenderVolumeLight(CommandBuffer cmd, MyRenderingData renderingData)
+		{
+			var rendererSize = new Vector2Int(renderingData.camera.pixelWidth, renderingData.camera.pixelHeight) /
+			                   asset.volumeResolutionScale;
+			var rt = IdentifierPool.Get();
+			cmd.GetTemporaryRT(rt, rendererSize.x, rendererSize.y, 0, FilterMode.Point, RenderTextureFormat.Default);
+			cmd.SetRenderTarget(rt, rt);
+			cmd.ClearRenderTarget(false, true, Color.black);
+
+			cmd.SetGlobalTexture("_CameraDepthTex", renderingData.depthTarget);
+			cmd.SetCameraParams(renderingData.camera, true);
+
+			float globalExtinction = Mathf.Log(10) / asset.visibilityDistance; //全局 每单位距离衰减
+
+			foreach (var volumeData in visibleVolumes)
+			{
+				if (!volumeData.volume.enabled)
+				{
+					return;
+				}
+
+				var light = renderingData.cullResults.visibleLights[volumeData.lightIndex];
+				Vector4 lightPos;
+				if (light.lightType == LightType.Directional)
+				{
+					lightPos = (-light.light.transform.forward).ToVector4(0);
+				}
+				else
+				{
+					lightPos = light.light.transform.position.ToVector4(1);
+				}
+
+				cmd.SetGlobalVector("_LightPosition", lightPos);
+				cmd.SetGlobalVector("_LightDirection", -light.light.transform.forward);
+				cmd.SetGlobalFloat("_LightAngle", Mathf.Cos(Mathf.Deg2Rad * light.spotAngle / 2));
+				cmd.SetGlobalVector("_LightColor", light.finalColor * volumeData.volume.intensityMultiplier);
+				cmd.SetGlobalVector("_WorldCameraPos", renderingData.camera.transform.position);
+				cmd.SetGlobalVector("_FrameSize",
+					new Vector4(rendererSize.x, rendererSize.y, 1f / rendererSize.x, 1f / rendererSize.y));
+				cmd.SetGlobalInt("_Steps", volumeData.volume.rayMarchingSteps);
+				cmd.SetGlobalVector("_RangeLimit", volumeData.volume.rayMarchingRange);
+				cmd.SetGlobalFloat("_IncomingLoss", volumeData.volume.incomingLoss);
+				cmd.SetGlobalFloat("_LightDistance", volumeData.volume.lightDistance);
+				var extinction = globalExtinction;
+				if (volumeData.volume.extinctionOverride)
+				{
+					extinction = Mathf.Log(10) / volumeData.volume.visibilityDistance;
+				}
+
+				cmd.SetGlobalVector("_TransmittanceExtinction", new Vector3(extinction, extinction, extinction));
+				if (asset.jitterPatterns.Length > 0)
+				{
+					cmd.SetGlobalTexture("_SampleNoise",
+						asset.jitterPatterns[renderingData.frameID % asset.jitterPatterns.Length]);
+				}
+
+				if (renderingData.shadowMapData.TryGetValue(volumeData.volume.TheLight, out var shadowData))
+				{
+					cmd.SetGlobalTexture("_ShadowMap", shadowData.shadowMapIdentifier);
+					cmd.SetGlobalMatrix("_WorldToLight", shadowData.world2Light);
+					cmd.SetGlobalFloat("_ShadowBias", shadowData.bias);
+					cmd.SetGlobalInt("_ShadowType", (int) shadowData.shadowType);
+					cmd.SetGlobalVector("_ShadowParameters", shadowData.shadowParameters);
+					cmd.SetGlobalMatrix("_ShadowPostTransform", shadowData.postTransform);
+					cmd.SetGlobalInt("_UseShadow", 1);
+				}
+				else
+				{
+					cmd.SetGlobalInt("_UseShadow", 0);
+				}
+
+				var boundaryPlanes = volumeData.volume.GetVolumeBoundFaces(renderingData.camera);
+				cmd.SetGlobalVectorArray("_BoundaryPlanes", boundaryPlanes);
+				cmd.SetGlobalInt("_BoundaryPlaneCount", boundaryPlanes.Count);
+
+				switch (light.lightType)
+				{
+					case LightType.Point:
+					case LightType.Spot:
+						cmd.DrawMesh(volumeData.volume.VolumeMesh, volumeData.volume.transform.localToWorldMatrix,
+							volumeMat, 0, volumeScatteringPass);
+						break;
+					case LightType.Directional:
+						cmd.BlitFullScreen(BuiltinRenderTextureType.None, rt, volumeMat,
+							fullScreenVolumeScatteringPass);
+						break;
+				}
+			}
+
+			cmd.SetGlobalTexture("_CameraDepthTex", renderingData.depthTarget);
+			cmd.SetGlobalFloat("_GlobalFogExtinction", globalExtinction);
+			cmd.SetGlobalColor("_AmbientLight", asset.fogLight);
+			cmd.BlitFullScreen(BuiltinRenderTextureType.None, renderingData.colorTarget, volumeMat, globalFogPass);
+
+			cmd.Blit(rt, renderingData.colorTarget, volumeMat, volumeResolvePass);
+
+			cmd.ReleaseTemporaryRT(rt);
+			IdentifierPool.Release(rt);
+		}
+	}
+}
---
+++ Assets/Scripts/MyRenderPipeline/RenderPass/VolumeLight/VolumeLightRenderer.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyRenderPipeline.RenderPass.VolumeLight
+{
+	[ExecuteInEditMode]
+	[RequireComponent(typeof(Light))]
+	public class VolumeLightRenderer : MonoBehaviour
+	{
+		public Vector2 rayMarchingRange = new Vector2(0, 100);
+		public int rayMarchingSteps = 8;
+		[Range(0, 1)] public float incomingLoss = 0;
+		public float lightDistance = 100;
+		public bool extinctionOverride = false;
+		public float visibilityDistance = 100;
+		public float intensityMultiplier = 1;
+
+		private List<Vector4> planes = new List<Vector4>(6);
+
+		private float previousAngle;
+		private float previousRange;
+
+		public Light TheLight { get; private set; }
+		public Mesh VolumeMesh { get; private set; }
+
+		private void Awake()
+		{
+			TheLight = GetComponent<Light>();
+			VolumeMesh = new Mesh();
+			UpdateMesh();
+			previousAngle = TheLight.spotAngle;
+			previousRange = TheLight.range;
+		}
+
+		private void Update()
+		{
+			if (TheLight.spotAngle != previousAngle || TheLight.range != previousRange)
+			{
+				previousAngle = TheLight.spotAngle;
+				previousRange = TheLight.range;
+				UpdateMesh();
+			}
+		}
+
+		private void Reset()
+		{
+			VolumeMesh.vertices = new Vector3[]
+			{
+				new Vector3(-1, -1, -1),
+				new Vector3(-1, 1, -1),
+				new Vector3(1, 1, -1),
+				new Vector3(1, -1, -1),
+				new Vector3(-1, -1, 1),
+				new Vector3(-1, 1, 1),
+				new Vector3(1, 1, 1),
+				new Vector3(1, -1, 1),
+			};
+
+			VolumeMesh.triangles = new int[]
+			{
+				0, 1, 2, 0, 2, 3,
+				0, 4, 5, 0, 5, 1,
+				1, 5, 6, 1, 6, 2,
+				2, 6, 7, 2, 7, 3,
+				0, 3, 7, 0, 7, 4,
+				4, 6, 5, 4, 7, 6,
+			};
+
+			VolumeMesh.RecalculateNormals();
+			UpdateMesh();
+		}
+
+		//灯光的椎体 mesh
+		private void UpdateMesh()
+		{
+			if (TheLight.type == LightType.Spot)
+			{
+				var tanFOV = Mathf.Tan(TheLight.spotAngle / 2 * Mathf.Deg2Rad);
+				var verts = new Vector3[]
+				{
+					new Vector3(0, 0, 0),
+					new Vector3(-tanFOV, -tanFOV, 1) * TheLight.range,
+					new Vector3(-tanFOV, tanFOV, 1) * TheLight.range,
+					new Vector3(tanFOV, tanFOV, 1) * TheLight.range,
+					new Vector3(tanFOV, -tanFOV, 1) * TheLight.range,
+				};
+
+				VolumeMesh.Clear();
+				VolumeMesh.vertices = verts;
+				VolumeMesh.triangles = new int[]
+				{
+					0, 1, 2,
+					0, 2, 3,
+					0, 3, 4,
+					0, 4, 1,
+					1, 4, 3,
+					1, 3, 2,
+				};
+				VolumeMesh.RecalculateNormals();
+			}
+		}
+
+		public List<Vector4> GetVolumeBoundFaces(Camera camera)
+		{
+			planes.Clear();
+			Matrix4x4 viewProjection = Matrix4x4.identity;
+			if (TheLight.type == LightType.Spot)
+			{
+				//如果是聚光灯  则用聚光灯的VP
+				//灯光是反的
+				viewProjection = Matrix4x4.Perspective(TheLight.spotAngle, 1, 0.03f, TheLight.range) *
+				                 Matrix4x4.Scale(new Vector3(1, 1, -1)) * TheLight.transform.worldToLocalMatrix;
+				var m0 = viewProjection.GetRow(0); //X偏移
+				var m1 = viewProjection.GetRow(1); //y偏移
+				var m2 = viewProjection.GetRow(2); //z偏移
+				var m3 = viewProjection.GetRow(3); //中心点
+
+				planes.Add(-(m3 + m0));
+				planes.Add(-(m3 - m0));
+				planes.Add(-(m3 + m1));
+				planes.Add(-(m3 - m1));
+				//planes.Add(-(m3 + m2)); //ignore near
+				planes.Add(-(m3 - m2));
+			}
+			else if (TheLight.type == LightType.Directional)
+			{
+				//如果是平行光 则用 摄像机的VP
+				viewProjection = camera.projectionMatrix * camera.worldToCameraMatrix;
+				var m2 = viewProjection.GetRow(2);
+				var m3 = viewProjection.GetRow(3);
+				planes.Add(-(m3 + m2)); //only near plane
+			}
+
+			return planes;
+		}
+
+		private void OnDrawGizmosSelected()
+		{
+			Gizmos.color = Color.cyan;
+			Gizmos.DrawWireMesh(VolumeMesh, 0, transform.position, transform.rotation, transform.localScale);
+		}
+	}
+}
