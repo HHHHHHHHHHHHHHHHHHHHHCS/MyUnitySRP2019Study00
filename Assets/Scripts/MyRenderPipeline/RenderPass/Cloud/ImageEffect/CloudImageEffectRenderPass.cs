d679ce9f482fedc71364f931466e2eebb854b178
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2021-03-09 11:34:06 +0800

    add

--- Assets/Scripts/MyRenderPipeline/RenderPass/Cloud/ImageEffect/CloudImageEffectRenderPass.cs
+++ Assets/Scripts/MyRenderPipeline/RenderPass/Cloud/ImageEffect/CloudImageEffectRenderPass.cs
@@ -8,13 +8,51 @@ namespace MyRenderPipeline.RenderPass.Cloud.ImageEffect
 	{
 		private const string k_CloudImageEffectPass = "CloudImageEffectPass";
 
+		
+		private static readonly int NoiseTex_ID = Shader.PropertyToID("_NoiseTex");
+		private static readonly int DetailNoiseTex_ID = Shader.PropertyToID("_DetailNoiseTex");
+		private static readonly int BlueNoise_ID = Shader.PropertyToID("_BlueNoise");
+		private static readonly int WeatherMap_ID = Shader.PropertyToID("_WeatherMap");
+		private static readonly int Scale_ID = Shader.PropertyToID("_Scale");
+		private static readonly int DensityMultiplier_ID = Shader.PropertyToID("_DensityMultiplier");
+		private static readonly int DensityOffset_ID = Shader.PropertyToID("_DensityOffset");
+		private static readonly int LightAbsorptionThroughCloud_ID = Shader.PropertyToID("_LightAbsorptionThroughCloud");
+		private static readonly int LightAbsorptionTowardSun_ID = Shader.PropertyToID("_LightAbsorptionTowardSun");
+		private static readonly int DarknessThreshold_ID = Shader.PropertyToID("_DarknessThreshold");
+		private static readonly int Params_ID = Shader.PropertyToID("_Params");
+		private static readonly int RayOffsetStrength_ID = Shader.PropertyToID("_RayOffsetStrength");
+		private static readonly int DetailNoiseScale_ID = Shader.PropertyToID("_DetailNoiseScale");
+		private static readonly int DetailNoiseWeight_ID = Shader.PropertyToID("_DetailNoiseWeight");
+		private static readonly int ShapeOffset_ID = Shader.PropertyToID("_ShapeOffset");
+		private static readonly int DetailOffset_ID = Shader.PropertyToID("_DetailOffset");
+		private static readonly int DetailWeights_ID = Shader.PropertyToID("_DetailWeights");
+		private static readonly int ShapeNoiseWeights_ID = Shader.PropertyToID("_ShapeNoiseWeights");
+		private static readonly int PhaseParams_ID = Shader.PropertyToID("_PhaseParams");
+		private static readonly int BoundsMin_ID = Shader.PropertyToID("_BoundsMin");
+		private static readonly int BoundsMax_ID = Shader.PropertyToID("_BoundsMax");
+		private static readonly int NumStepsLight_ID = Shader.PropertyToID("_NumStepsLight");
+		private static readonly int MapSize_ID = Shader.PropertyToID("_MapSize");
+		private static readonly int TimeScale_ID = Shader.PropertyToID("_TimeScale");
+		private static readonly int BaseSpeed_ID = Shader.PropertyToID("_BaseSpeed");
+		private static readonly int DetailSpeed_ID = Shader.PropertyToID("_DetailSpeed");
+		private static readonly int ColA_ID = Shader.PropertyToID("_ColA");
+		private static readonly int ColB_ID = Shader.PropertyToID("_ColB");
+		private static readonly int DebugViewMode_ID = Shader.PropertyToID("_DebugViewMode");
+		private static readonly int DebugNoiseSliceDepth_ID = Shader.PropertyToID("_DebugNoiseSliceDepth");
+		private static readonly int DebugTileAmount_ID = Shader.PropertyToID("_DebugTileAmount");
+		private static readonly int ViewerSize_ID = Shader.PropertyToID("_ViewerSize");
+		private static readonly int DebugChannelWeight_ID = Shader.PropertyToID("_DebugChannelWeight");
+		private static readonly int DebugGreyscale_ID = Shader.PropertyToID("_DebugGreyscale");
+		private static readonly int DebugShowAllChannels_ID = Shader.PropertyToID("_DebugShowAllChannels");
+
+		
 		private Material material;
 		private Texture2D blueNoise;
 
 		private WeatherMap weatherMap;
 		private NoiseGenerator noiseGenerator;
 		private ContainerVis containerVis;
-
+		
 		public void Init(Material material, Texture2D blueNoise)
 		{
 			this.material = material;
@@ -73,51 +111,54 @@ namespace MyRenderPipeline.RenderPass.Cloud.ImageEffect
 			
 			using (new ProfilingScope(cmd, sampler))
 			{
-				material.SetTexture("NoiseTex", noiseGenerator.shapeTexture);
-				material.SetTexture("DetailNoiseTex", noiseGenerator.detailTexture);
-				material.SetTexture("BlueNoise", blueNoise);
-				material.SetTexture("WeatherMap", weatherMap.weatherMap);
+				material.SetTexture(NoiseTex_ID, noiseGenerator.shapeTexture);
+				material.SetTexture(DetailNoiseTex_ID, noiseGenerator.detailTexture);
+				material.SetTexture(BlueNoise_ID, blueNoise);
+				material.SetTexture(WeatherMap_ID, weatherMap.weatherMap);
 
-				var container = containerVis.transform;
-				Vector3 size = containerVis.transform.lossyScale;
-				int width = Mathf.CeilToInt(size.x);
-				int height = Mathf.CeilToInt(size.y);
-				int depth = Mathf.CeilToInt(size.z);
 
-				material.SetFloat("scale", settings.cloudScale.value);
-				material.SetFloat("densityMultiplier", settings.densityMultiplier.value);
-				material.SetFloat("densityOffset", settings.densityOffset.value);
-				material.SetFloat("lightAbsorptionThroughCloud", settings.lightAbsorptionThroughCloud.value);
-				material.SetFloat("lightAbsorptionTowardSun", settings.lightAbsorptionTowardSun.value);
-				material.SetFloat("darknessThreshold", settings.darknessThreshold.value);
-				material.SetVector("params", settings.cloudTestParams.value);
-				material.SetFloat("rayOffsetStrength", settings.rayOffsetStrength.value);
 
-				material.SetFloat("detailNoiseScale", settings.detailNoiseScale.value);
-				material.SetFloat("detailNoiseWeight", settings.detailNoiseWeight.value);
-				material.SetVector("shapeOffset", settings.shapeOffset.value);
-				material.SetVector("detailOffset", settings.detailOffset.value);
-				material.SetVector("detailWeights", settings.detailNoiseWeights.value);
-				material.SetVector("shapeNoiseWeights", settings.shapeNoiseWeights.value);
-				material.SetVector("phaseParams",
+				material.SetFloat(Scale_ID, settings.cloudScale.value);
+				material.SetFloat(DensityMultiplier_ID, settings.densityMultiplier.value);
+				material.SetFloat(DensityOffset_ID, settings.densityOffset.value);
+				material.SetFloat(LightAbsorptionThroughCloud_ID, settings.lightAbsorptionThroughCloud.value);
+				material.SetFloat(LightAbsorptionTowardSun_ID, settings.lightAbsorptionTowardSun.value);
+				material.SetFloat(DarknessThreshold_ID, settings.darknessThreshold.value);
+				material.SetVector(Params_ID, settings.cloudTestParams.value);
+				material.SetFloat(RayOffsetStrength_ID, settings.rayOffsetStrength.value);
+
+				material.SetFloat(DetailNoiseScale_ID, settings.detailNoiseScale.value);
+				material.SetFloat(DetailNoiseWeight_ID, settings.detailNoiseWeight.value);
+				material.SetVector(ShapeOffset_ID, settings.shapeOffset.value);
+				material.SetVector(DetailOffset_ID, settings.detailOffset.value);
+				material.SetVector(DetailWeights_ID, settings.detailNoiseWeights.value);
+				material.SetVector(ShapeNoiseWeights_ID, settings.shapeNoiseWeights.value);
+				material.SetVector(PhaseParams_ID,
 					new Vector4(settings.forwardScattering.value,
 						settings.backScattering.value, settings.baseBrightness.value, settings.phaseFactor.value));
 
-				material.SetVector("boundsMin", container.position - container.localScale / 2);
-				material.SetVector("boundsMax", container.position + container.localScale / 2);
+				var container = containerVis.transform;
+				Vector3 pos = container.position;
+				Vector3 size = container.lossyScale;
+				int width = Mathf.CeilToInt(size.x);
+				int height = Mathf.CeilToInt(size.y);
+				int depth = Mathf.CeilToInt(size.z);
+				
+				material.SetVector(BoundsMin_ID, pos - size / 2);
+				material.SetVector(BoundsMax_ID, pos + size / 2);
 
-				material.SetInt("numStepsLight", settings.numStepsLight.value);
+				material.SetVector(MapSize_ID, new Vector4(width, height, depth, 0));
 
-				material.SetVector("mapSize", new Vector4(width, height, depth, 0));
-
-				material.SetFloat("timeScale", (Application.isPlaying) ? settings.timeScale.value : 0);
-				material.SetFloat("baseSpeed", settings.baseSpeed.value);
-				material.SetFloat("detailSpeed", settings.detailSpeed.value);
+				material.SetInt(NumStepsLight_ID, settings.numStepsLight.value);
+				
+				material.SetFloat(TimeScale_ID, (Application.isPlaying) ? settings.timeScale.value : 0);
+				material.SetFloat(BaseSpeed_ID, settings.baseSpeed.value);
+				material.SetFloat(DetailSpeed_ID, settings.detailSpeed.value);
 
 				SetDebugParams();
 
-				material.SetColor("colA", settings.colA.value);
-				material.SetColor("colB", settings.colB.value);
+				material.SetColor(ColA_ID, settings.colA.value);
+				material.SetColor(ColB_ID, settings.colB.value);
 
 
 
@@ -143,13 +184,13 @@ namespace MyRenderPipeline.RenderPass.Cloud.ImageEffect
 				debugModeIndex = 3;
 			}
 
-			material.SetInt("debugViewMode", debugModeIndex);
-			material.SetFloat("debugNoiseSliceDepth", noiseGenerator.viewerSliceDepth);
-			material.SetFloat("debugTileAmount", noiseGenerator.viewerTileAmount);
-			material.SetFloat("viewerSize", noiseGenerator.viewerSize);
-			material.SetVector("debugChannelWeight", noiseGenerator.ChannelMask);
-			material.SetInt("debugGreyscale", (noiseGenerator.viewerGreyscale) ? 1 : 0);
-			material.SetInt("debugShowAllChannels", (noiseGenerator.viewerShowAllChannels) ? 1 : 0);
+			material.SetInt(DebugViewMode_ID, debugModeIndex);
+			material.SetFloat(DebugNoiseSliceDepth_ID, noiseGenerator.viewerSliceDepth);
+			material.SetFloat(DebugTileAmount_ID, noiseGenerator.viewerTileAmount);
+			material.SetFloat(ViewerSize_ID, noiseGenerator.viewerSize);
+			material.SetVector(DebugChannelWeight_ID, noiseGenerator.ChannelMask);
+			material.SetInt(DebugGreyscale_ID, (noiseGenerator.viewerGreyscale) ? 1 : 0);
+			material.SetInt(DebugShowAllChannels_ID, (noiseGenerator.viewerShowAllChannels) ? 1 : 0);
 		}
 	}
 }
---
+++ Assets/Shaders/Cloud/ImageEffectCloud.shader
@@ -0,0 +1,185 @@
+﻿Shader "MyRP/Cloud/ImageEffectCloud"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" { }
+	}
+	SubShader
+	{
+		Cull Off
+		ZWrite Off
+		ZTest Always
+		
+		Pass
+		{
+			HLSLPROGRAM
+			
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+			#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareOpaqueTexture.hlsl"
+			#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareDepthTexture.hlsl"
+			
+			struct a2v
+			{
+				float4 vertex: POSITION;
+				float2 uv: TEXCOORD0;
+			};
+			
+			struct v2f
+			{
+				float4 pos: SV_POSITION;
+				float2 uv: TEXCOORD0;
+				float3 viewVector: TEXCOORD1;
+			};
+			
+			
+			// Textures
+			TEXTURE3D_PARAM(_NoiseTex, sampler_NoiseTex);
+			TEXTURE3D_PARAM(_DetailNoiseTex, sampler_DetailNoiseTex);
+			TEXTURE3D_PARAM(_WeatherMap, sampler_WeatherMap);
+			TEXTURE3D_PARAM(_BlueNoise, sampler_BlueNoise);
+			
+			// Shape settings
+			float4 _Params;
+			int3 _MapSize;
+			float _DensityMultiplier;
+			float _DensityOffset;
+			float _Scale;
+			float _DetailNoiseScale;
+			float _DetailNoiseWeight;
+			float3 _DetailWeights;
+			float4 _ShapeNoiseWeights;
+			float4 _PhaseParams;
+			
+			// March settings
+			int _NumStepsLight;
+			float _RayOffsetStrength;
+			
+			float3 _BoundsMin;
+			float3 _BoundsMax;
+			
+			float3 _ShapeOffset;
+			float3 _DetailOffset;
+			
+			// Light settings
+			float _LightAbsorptionTowardSun;
+			float _LightAbsorptionThroughCloud;
+			float _DarknessThreshold;
+			float4 _ColA;
+			float4 _ColB;
+			
+			// Animation settings
+			float _TimeScale;
+			float _BaseSpeed;
+			float _DetailSpeed;
+			
+			// Debug settings:
+			int _DebugViewMode; // 0 = off; 1 = shape tex; 2 = detail tex; 3 = weathermap
+			int _DebugGreyscale;
+			int _DebugShowAllChannels;
+			float _DebugNoiseSliceDepth;
+			float4 _DebugChannelWeight;
+			float _DebugTileAmount;
+			float _ViewerSize;
+			
+			float Remap(float v, float minOld, float maxOld, float minNew, float maxNew)
+			{
+				return minNew + (v - minOld) * (maxNew - minNew) / (maxOld - minOld);
+			}
+			
+			float Remap01(float v, float low, float high)
+			{
+				return(v - low) / (high - low);
+			}
+			
+			float2 SquareUV(float2 uv)
+			{
+				return uv.xy * _ScreenParams.xy / 1000.0;
+			}
+			
+			// Returns (dstToBox, dstInsideBox). If ray misses box, dstInsideBox will be zero
+			// https://zhuanlan.zhihu.com/p/125834180
+			float2 RayBoxDst(float3 boundsMin, float3 boundsMax, float3 rayOrigin, float3 invRayDir)
+			{
+				// Adapted from: http://jcgt.org/published/0007/03/04/
+				float3 t0 = (boundsMin - rayOrigin) * invRayDir;
+				float3 t1 = (boundsMax - rayOrigin) * invRayDir;
+				
+				float3 tmin = min(t0, t1);
+				float3 tmax = max(t0, t1);
+				
+				float dstA = max(max(tmin.x, tmin.y), tmin.z);
+				float dstB = min(min(tmax.x, tmax.y), tmax.z);
+				
+				// CASE 1: ray intersects box from outside (0 <= dstA <= dstB)
+				// dstA is dst to nearest intersection, dstB dst to far intersection
+				
+				// CASE 2: ray intersects box from inside (dstA < 0 < dstB)
+				// dstA is the dst to intersection behind the ray, dstB is dst to forward intersection
+				
+				// CASE 3: ray misses box (dstA > dstB)
+				
+				float dstToBox = max(0, dstA);
+				float dstInsideBox = max(0, dstB - dstToBox);
+				return float2(dstToBox, dstInsideBox);
+			}
+			
+			// Henyey-Greenstein
+			// https://zhuanlan.zhihu.com/p/34836881
+			float HG(float a, float g)
+			{
+				float g2 = g * g;
+				return(1 - g2) / (4 * PI * pow(1 + g2 - 2 * g * a, 1.5));
+			}
+			
+			float Phase(float a)
+			{
+				float blend = 0.5;
+				float hgBlend = HG(a, _PhaseParams.x) * (1 - blend) + hgBlend(a, -_PhaseParams.y) * blend;
+			}
+			
+			float Beer(float d)
+			{
+				float beer = exp(-d);
+				return beer;
+			}
+			
+			float SampleDensity(float3 rayPos)
+			{
+				// Constants:
+				const int mipLevel = 0;
+				const float baseScale = 1 / 1000.0;
+				const float offsetSpeed = 1 / 100.0;
+
+				// Calculate texture sample positions
+				float time = _Time.x * _TimeScale;
+				//TODO:
+			}
+			
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.pos = TransformObjectToHClip(v.vertex);
+				o.uv = v.uv;
+				// Camera space matches OpenGL convention where cam forward is -z. In unity forward is positive z.
+				// (https://docs.unity3d.com/ScriptReference/Camera-cameraToWorldMatrix.html)
+				float3 viewVector = mul(unity_CameraInvProjection, float4(v.uv * 2 - 1, 0, -1));
+				o.viewVector = mul(unity_CameraToWorld, float4(viewVector, 0));
+				return o;
+			}
+			
+			fixed4 frag(v2f i): SV_Target
+			{
+				// sample the texture
+				fixed4 col = tex2D(_MainTex, i.uv);
+				// apply fog
+				UNITY_APPLY_FOG(i.fogCoord, col);
+				return col;
+			}
+			ENDHLSL
+			
+		}
+	}
+}
